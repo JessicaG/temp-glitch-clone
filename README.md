0e923673eebf56bde09d09b4a862d2beed554cf3
Glitch (ld-dashboard) <none>
2020-04-16 04:13:43 +0000

    🎄🏝 init project

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-#test
+Dashboard Example
+========================
+
+## Getting Started
+- You'll need `CLIENT_ID` and `CLIENT_SECRET` for the Google account
+- npm install
+
+## Snowflake
+
+## Charts
+
+Spread some coding love
+-----------------
+(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
---
+++ server.js
@@ -0,0 +1,96 @@
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+// the process.env values are set in .env
+passport.use(new GoogleStrategy({
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: 'https://'+process.env.PROJECT_DOMAIN+'.glitch.me/login/google/return',
+  scope: 'https://www.googleapis.com/auth/plus.login'
+},
+function(token, tokenSecret, profile, cb) {
+  return cb(null, profile);
+}));
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+// init project
+var express = require('express');
+var app = express();
+var expressSession = require('express-session');
+
+// cookies are used to save authentication
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(expressSession({ secret:'watchingfairies', resave: true, saveUninitialized: true, maxAge: (90 * 24 * 3600000) }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// index route
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// on clicking "logoff" the cookie is cleared
+app.get('/logoff',
+  function(req, res) {
+    res.clearCookie('google-passport-example');
+    res.redirect('/');
+  }
+);
+
+app.get('/auth/google', passport.authenticate('google'));
+
+app.get('/login/google/return', 
+  passport.authenticate('google', 
+    { successRedirect: '/setcookie', failureRedirect: '/' }
+  )
+);
+
+// on successful auth, a cookie is set before redirecting
+// to the success view
+app.get('/setcookie', requireUser,
+  function(req, res) {
+    if(req.get('Referrer') && req.get('Referrer').indexOf("google.com")!=-1){
+      res.cookie('google-passport-example', new Date());
+      res.redirect('/success');
+    } else {
+       res.redirect('/');
+    }
+  }
+);
+
+// if cookie exists, success. otherwise, user is redirected to index
+app.get('/success', requireLogin,
+  function(req, res) {
+    res.sendFile(__dirname + '/views/success.html');
+  }
+);
+
+function requireLogin (req, res, next) {
+  if (!req.cookies['google-passport-example']) {
+    res.redirect('/');
+  } else {
+    next();
+  }
+};
+
+function requireUser (req, res, next) {
+  if (!req.user) {
+    res.redirect('/');
+  } else {
+    next();
+  }
+};
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
