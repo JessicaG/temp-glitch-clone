ff4000339f22d1c3cb771ddf4a96a44abfdfec61
Traci Lopez <traci@launchdarkly.com>
2020-04-17 12:12:52 -0700

    use csv data

--- public/js/success_charts.js
+++ public/js/success_charts.js
@@ -1,19 +1,26 @@
-var ctx = document.getElementById('chart').getContext('2d'); //from the docs https://www.chartjs.org/docs/latest/getting-started/
-var chart = new Chart(ctx, {
+d3.csv('sdks-used-trial-accounts.csv')
+    .then(makeChart);
+
+function makeChart(sdks) {
+    var labels = sdks.map(sdk => sdk.SDK)
+    var distinctCounts = sdks.map(sdk => sdk.COUNT)
+    var ctx = document.getElementById('chart').getContext('2d'); //from the docs https://www.chartjs.org/docs/latest/getting-started/
+    var chart = new Chart(ctx, {
     // not sure what charts we like but this is is a start
     type: 'line',
 
     // Fake data
     data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        labels,
         datasets: [{
-            label: 'data',
+            label: 'distinct sdks used in trial accounts',
             backgroundColor: 'rgb(255, 99, 132)',
             borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
+            data: distinctCounts
         }]
     },
 
     // Config options go here (not sure what we want)
     options: {}
 });
+}
